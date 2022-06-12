047989cbab9aa0d6a09ff9f950354c616368645a
harrika <harrikareddy5@gmail.com>
2022-06-12 12:38:47 +0530

    feat:creating new app

---
+++ screens/Allexpenses.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Allexpenses = () => {
+  return (
+    <View>
+      <Text>Allexpenses</Text>
+    </View>
+  )
+}
+
+export default Allexpenses
+
+const styles = StyleSheet.create({})
